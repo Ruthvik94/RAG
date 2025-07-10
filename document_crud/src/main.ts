162026a44249177b1b9aac96e7b5eab4df194bed
Ruthvik M <ruthvikdm@gmail.com>
2025-07-10 13:34:19 +0530

    Update docker file for rag_ui

--- document_crud/src/main.ts
+++ document_crud/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: "http://localhost:5173",
+    origin: `http://localhost:${process.env.RAG_UI_PORT}`,
     credentials: true,
   });
 
