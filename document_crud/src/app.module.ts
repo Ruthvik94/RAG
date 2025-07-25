00ac871500c576b04ca200f1b38cab11d3a3f314
Ruthvik M <ruthvikdm@gmail.com>
2025-07-25 20:58:18 +0530

    Remove ORM config

--- document_crud/src/app.module.ts
+++ document_crud/src/app.module.ts
@@ -1,15 +1,7 @@
 import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
-// import { DocumentsModule } from "./documents/documents.module";
-import ormconfig from "./ormconfig";
-// import { PythonModule } from './python-redis/python-redis.module';
 import { PythonModule } from "./python-redis/python-upstash.module";
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot(ormconfig.options),
-    // DocumentsModule,
-    PythonModule,
-  ],
+  imports: [PythonModule],
 })
 export class AppModule {}
